69dba76e48f783e587f9510c8a3cb9397ab8f79c
cykuo <cykuotw45@gmail.com>
2023-02-03 14:53:25 -0500

    [feat] Red-Black Tree struct

---
+++ redBlackTree/redBlackTree.go
@@ -0,0 +1,31 @@
+package redblacktree
+
+type color bool
+
+const (
+	BLACK, RED color = true, false
+)
+
+type TreeNode struct {
+	Val    int
+	Key    int
+	color  color
+	Parent *TreeNode
+	Left   *TreeNode
+	Right  *TreeNode
+}
+
+type RedBlackTree struct {
+	Root *TreeNode
+}
+
+/*
+Red Black Tree Properties
+1. Every node is either red or black
+2. The root is black
+3. Every leaf (nil) is black
+4. If a node is red, both of its children are black
+	(aka no consecutive 2 red nodes)
+5. For each node, all paths from the node to the leaf (nil)
+	passes through exact the same number of black nodes
+*/
