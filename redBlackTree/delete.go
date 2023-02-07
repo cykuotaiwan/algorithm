84eaee7f8616df362a383eb486834b2279e9ef30
cykuo <cykuotw45@gmail.com>
2023-02-06 20:53:35 -0500

    [feat] Red-Black Tree delete fix

--- redBlackTree/delete.go
+++ redBlackTree/delete.go
@@ -38,5 +38,79 @@ func (tree *RedBlackTree) Delete(key int) {
 }
 
 func (tree *RedBlackTree) deleteFix(node *TreeNode) {
+	for node != tree.Root && node.color == BLACK {
+		if node == node.siblingLeft() {
+			// case 1
+			//     node is the left child
+			sibling := node.siblingRight()
+			if sibling.color == RED {
+				sibling.color = BLACK
+				node.Parent.color = RED
+				tree.rotateLeft(node.Parent)
+				sibling = node.Parent.Right
+			}
 
+			if sibling.Left.color == BLACK && sibling.Right.color == BLACK {
+				// case 2
+				//     node's sibling is black
+				//     and both of the sibling's children are also black
+				sibling.color = RED
+				node = node.Parent
+			} else if sibling.Right.color == BLACK {
+				// case 3
+				//     node's sibling is black
+				//     and sibling's left child is red, right child is black
+				sibling.Left.color = BLACK
+				sibling.color = RED
+				tree.rotateRight(sibling)
+				sibling = node.Parent.Right
+			}
+			// case 4
+			//     node's sibling is black
+			//     and sibling's right child is red
+			sibling.color = node.Parent.color
+			node.Parent.color = BLACK
+			sibling.Right.color = BLACK
+			tree.rotateLeft(node.Parent)
+			node = tree.Root
+		} else {
+			if node == node.siblingRight() {
+				// case 1
+				//     node is the right child
+				sibling := node.siblingLeft()
+				if sibling.color == RED {
+					sibling.color = BLACK
+					node.Parent.color = RED
+					tree.rotateRight(node.Parent)
+					sibling = node.Parent.Left
+				}
+
+				if sibling.Right.color == BLACK && sibling.Left.color == BLACK {
+					// case 2
+					//     node's sibling is black
+					//     and both of the children are also black
+					sibling.color = RED
+					node = node.Parent
+				} else if sibling.Left.color == BLACK {
+					// case 3
+					//     node's sibling is black
+					//     and sibling's left child is red, right child is black
+					sibling.Right.color = BLACK
+					sibling.color = RED
+					tree.rotateLeft(sibling)
+					sibling = node.Parent.Left
+				}
+
+				// case 4
+				//     node's sibling is black
+				//     and sibling's right child is red
+				sibling.color = node.Parent.color
+				node.Parent.color = BLACK
+				sibling.Left.color = BLACK
+				tree.rotateRight(node.Parent)
+				node = tree.Root
+			}
+		}
+	}
+	node.color = BLACK
 }
