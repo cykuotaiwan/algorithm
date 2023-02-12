d30d1fa7feeffae3cedb178f44a6fce439f54490
cykuo <cykuotw45@gmail.com>
2023-02-12 18:39:34 -0500

    [fix] rotate

--- redBlackTree/rotate.go
+++ redBlackTree/rotate.go
@@ -1,43 +1,40 @@
 package redBlackTree
 
+func (tree *RedBlackTree) replaceNode(old *TreeNode, new *TreeNode) {
+	if old.Parent == nil {
+		tree.Root = new
+		return
+	}
+
+	if old == old.Parent.Left { // node is left child
+		old.Parent.Left = new
+	} else { // node is right child
+		old.Parent.Right = new
+	}
+
+	if new != nil {
+		new.Parent = old.Parent
+	}
+}
+
 func (tree *RedBlackTree) rotateLeft(node *TreeNode) {
 	right := node.Right
-
+	tree.replaceNode(node, right)
 	node.Right = right.Left
 	if right.Left != nil {
 		right.Left.Parent = node
 	}
-
-	right.Parent = node.Parent
-	if node.Parent != nil { // node is root
-		tree.Root = right
-	} else if node == node.Parent.Left { // node is left child
-		node.Parent.Left = right
-	} else { // node is right child
-		node.Parent.Right = right
-	}
-
 	right.Left = node
 	node.Parent = right
 }
 
 func (tree *RedBlackTree) rotateRight(node *TreeNode) {
 	left := node.Left
-
+	tree.replaceNode(node, left)
 	node.Left = left.Right
 	if left.Right != nil {
 		left.Right.Parent = node
 	}
-
-	left.Parent = node.Parent
-	if node.Parent != nil { // node is root
-		tree.Root = left
-	} else if node == node.Parent.Left { // node is left child
-		node.Parent.Left = left
-	} else { // node is right child
-		node.Parent.Right = left
-	}
-
 	left.Right = node
 	node.Parent = left
 }
