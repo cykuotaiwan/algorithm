1fe0fd7c38a1a44eeb6c14315ba548908a1674aa
cykuo <cykuotw45@gmail.com>
2023-02-15 19:35:44 -0500

    [test] add delete test

--- redBlackTree/redBlackTree_test.go
+++ redBlackTree/redBlackTree_test.go
@@ -18,3 +18,22 @@ func TestRedBlackTreeInsert(t *testing.T) {
 
 	tree.Visualize("testInsert")
 }
+
+func TestRedBlackTreeDelete(t *testing.T) {
+	// generate rbt
+	var tree *rbt.RedBlackTree
+
+	tree.Insert(12, 12)
+	tree.Insert(7, 7)
+	tree.Insert(16, 16)
+	tree.Insert(4, 4)
+	tree.Insert(6, 6)
+	tree.Insert(8, 8)
+	tree.Insert(9, 9)
+	tree.Insert(10, 10)
+
+	t.Run("normal delete", func(t *testing.T) {
+		tree.Delete(6)
+		tree.Visualize("testDelete")
+	})
+}
